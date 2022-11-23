f48956a1062b3fcb8fb953ad72650a233181c4ba
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-11-23 07:32:10 +0100

    extracted the down arrow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import '../app.css'
+  import Arrow from './Arrow.svelte'
 </script>
 
 
@@ -7,18 +8,7 @@
   <div class="overflow-hidden w-vw h-vw relative">
     <slot/>
     <div class="absolute text-xl bottom-3 left-0 right-0 flex justify-center items-center gap-4">
-      <button class="w-16 h-16 rounded-full bg-white/30 hover:bg-white/40 p-3">
-        <svg xmlns="http://www.w3.org/2000/svg" class="" viewBox="2 1 26 30">
-          <path
-            d="M 15 4 v 22 l-10 -10 l10 10 l10 -10"
-            stroke="white"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            fill="none"
-          />
-        </svg>
-      </button>
+      <Arrow/>
       <div class="">Navigation</div>
     </div>
   </div>
---
+++ src/routes/Arrow.svelte
@@ -0,0 +1,12 @@
+<button class="w-16 h-16 rounded-full bg-white/30 hover:bg-white/40 p-3">
+  <svg xmlns="http://www.w3.org/2000/svg" class="" viewBox="2 1 26 30">
+    <path
+      d="M 15 4 v 22 l-10 -10 l10 10 l10 -10"
+      stroke="white"
+      stroke-width="4"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      fill="none"
+    />
+  </svg>
+</button>
