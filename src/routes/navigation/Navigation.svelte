eb3be4ee98aa27b89cc234f86023510f5d1c0480
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-11-25 16:19:38 +0100

    changed the minimap style

--- src/routes/navigation/Navigation.svelte
+++ src/routes/navigation/Navigation.svelte
@@ -2,8 +2,8 @@
   <div class="p-[5vw]"/>
   <h1 class="text-[15vw]">Navigation</h1>
   <div class="p-[3vw]"/>
-  <div class="mini-map grid w-[60vw]">
-    <div class="face" style="grid-area: title">Minimap</div>
+  <div class="minimap grid w-[60vw]">
+    <div class="text-[4vw] grid items-end p-[1.2vw]" style="grid-area: title">Minimap</div>
     <div class="face bg-blue-600"                   style="grid-area: blue">Landing Page</div>
     <div class="face bg-yellow-400 text-zinc-900" style="grid-area: yellow">About Me</div>
     <div class="face"                                 style="grid-area: orange">Navigation</div>
@@ -14,11 +14,11 @@
 </div>
 
 <style>
-  .mini-map {
+  .minimap {
     grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-template-rows: 1fr 1fr 1fr;
     grid-template-areas:
-      "title  blue   .     ."
+      ".      blue   title title"
       "yellow orange white red"
       ".      green  .     .";
   }
