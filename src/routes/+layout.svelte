1ff1cbbf8e11b815836e709c63e88edf74cfca13
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-11-29 22:52:56 +0100

    added language switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   import {beforeNavigate} from '$app/navigation'
+  import {browser} from '$app/environment'
+  import {page} from '$app/stores'
+
   import Transition from '$lib/Transition.svelte'
   import {directionMap} from '$lib/constants'
-  import {setLocale} from '$i18n/i18n-svelte'
   import '../app.css'
-
+  
+  import {setLocale, locale} from '$i18n/i18n-svelte'
+  import {loadLocaleAsync} from '$i18n/i18n-util.async'
+  import {locales} from '$i18n/i18n-util'
+  
   import type {LayoutData} from './$types'
 
   export let data: LayoutData
@@ -20,7 +26,36 @@
 
     direction = directionMap[fromStr]?.[toStr] ?? 'down'
   })
+
+
+  const switchLocale = async (newLocale: Locales, updateHistoryState = true) => {
+		if ($locale === newLocale)
+      return
+
+		await loadLocaleAsync(newLocale)
+		setLocale(newLocale)
+		document.querySelector('html')!.setAttribute('lang', newLocale)
+
+    if (updateHistoryState) {
+			history.pushState({ locale: newLocale }, '', replaceLocaleInUrl(location.pathname, newLocale))
+		}
+	}
+
+  const replaceLocaleInUrl = (path: string, locale: string): string => {
+    const [, , ...rest] = path.split('/')
+    return `/${[locale, ...rest].join('/')}`
+  }
+
+	const handlePopStateEvent = async ({ state }: PopStateEvent) => switchLocale(state.locale, false)
+
+  $: if (browser) {
+		const lang = $page.params.lang as Locales
+		switchLocale(lang, false)
+		history.replaceState({ ...history.state, locale: lang }, '', replaceLocaleInUrl(location.pathname, lang))
+	}
 </script>
+
+<svelte:window on:popstate={handlePopStateEvent}/>
 
 <main class="h-screen grid place-items-center">
   <div class="relative w-vw h-vw overflow-hidden">
@@ -29,5 +64,14 @@
         <slot/>
       </div>
     </Transition>
+  </div>
+
+  <div class="fixed top-4 left-4 flex gap-2">
+    {#each locales as l (l)}
+      <button
+        class="w-14 h-14 rounded-full bg-white/20 uppercase {$locale === l && 'bg-white/40'}"
+        on:click={() => switchLocale(l)}
+      >{l}</button>
+    {/each}
   </div>
 </main>
--- src/routes/[lang]/cube/+page.svelte
+++ src/routes/[lang]/cube/+page.svelte
@@ -2,9 +2,6 @@
   import {onMount} from 'svelte'
   import {init} from '$lib/cube'
 
-  import {setLocale, locale} from '$i18n/i18n-svelte'
-  import {locales} from '$i18n/i18n-util'
-
   let wrapper: HTMLElement
 
   onMount(() => {
@@ -12,13 +9,4 @@
   })
 </script>
 
-<div class="fixed inset-0" bind:this={wrapper}>
-  <div class="absolute top-4 left-4 flex gap-2">
-    {#each locales as l (l)}
-      <button
-        class="w-14 h-14 rounded-full bg-white/20 uppercase {$locale === l && 'bg-white/40'}"
-        on:click={() => setLocale(l)}
-      >{l}</button>
-    {/each}
-  </div>
-</div>
+<div class="fixed inset-0" bind:this={wrapper}></div>
